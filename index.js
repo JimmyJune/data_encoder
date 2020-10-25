6955d08eec08795f7cae5f6954c3ed2985cb14e8
yujingming@gmail.com <yujingming@gmail.com>
2020-10-25 21:51:11 +0800

    变更输入和输出参数

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 var AES = require("crypto-js/aes");
 const fs = require('fs');
 const encrypt = require('./encrypt');
+var path = require("path")
 
 if (process.argv.length != 4){
-    console.log('node index.js path_to_recon_json path_to_model_file');
+    console.log('node index.js input_dir output_dir');
+    console.log('input dir where deploy.json and model.obj ara')
     process.exit();
 }
+
+let input_dir = process.argv[2];
+let output_dir = process.argv[3];
     
-let recon_json = process.argv[2];
-let model_file = process.argv[3];
+let recon_json = path.join(input_dir, 'deploy.json');
+let model_file = path.join(input_dir, 'model.obj');
 
 console.log(recon_json, model_file);
+
+let output_recon_json = path.join(output_dir, 'deploy2.bin');
+let output_model_file = path.join(output_dir, 'model.model');
 
 key = 'RNWHLMuDrEpW00z0';
 
 fs.readFile(recon_json, 'utf8', function(err, data){
     // console.log(data);  
     let jm = encrypt.Encrypt(data, key);
-    fs.writeFileSync('deploy2.bin', jm)
+    fs.writeFileSync(output_recon_json, jm)
 });
 
 fs.readFile(model_file, 'utf8', function(err, data){
     // console.log(data);  
     let jm = encrypt.Encrypt(data, key);
-    fs.writeFileSync('model.model', jm)
+    fs.writeFileSync(output_model_file, jm)
 });
 
 
